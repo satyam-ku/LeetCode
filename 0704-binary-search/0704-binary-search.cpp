53221200dc7bec017cc2c49084dc12e7375968ca
Satyam Kumar <151944123+satyam-ku@users.noreply.github.com>
2026-02-07 00:44:22 +0530

    Time: 0 ms (100%), Space: 31.4 MB (7.42%) - LeetHub

--- 0704-binary-search/0704-binary-search.cpp
+++ 0704-binary-search/0704-binary-search.cpp
@@ -5,7 +5,7 @@ public:
         int r = nums.size()-1;
         
         while(l<=r){
-            int mid = (l+r)/2;
+            int mid = l+(r-l)/2;
             if(nums[mid] == target){ return mid;}
             else if(nums[mid]<target) {l = mid+1;}
             else {r = mid-1;} 
