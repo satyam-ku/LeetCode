03407ea01b4f46786095121803ad7cb4c3004cd2
Satyam Kumar <151944123+satyam-ku@users.noreply.github.com>
2026-02-08 22:40:32 +0530

    Time: 1 ms (2.84%), Space: 63.3 MB (97.12%) - LeetHub

--- 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
+++ 0852-peak-index-in-a-mountain-array/0852-peak-index-in-a-mountain-array.cpp
@@ -3,26 +3,26 @@ public:
     int peakIndexInMountainArray(vector<int>& arr) {
         int n = arr.size();
         int idx =-1;
-        // for(int i=1;i<=n-2;i++){
-        //     if(arr[i-1]<arr[i] && arr[i]>arr[i+1]){
-        //         idx = i;
-        //         break;
-        //     }
-        // }
-        int l = 1;
-        int r = n-2;
-        while(l<=r){
-            int mid = l + (r-l)/2;
-            if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
-                idx = mid;
-                return mid;
-            }
-            else if(arr[mid]>arr[mid+1]){
-                r = mid -1;
-            }else{
-                l =mid+1;
+        for(int i=1;i<=n-2;i++){
+            if(arr[i-1]<arr[i] && arr[i]>arr[i+1]){
+                idx = i;
+                break;
             }
         }
+        // int l = 1;
+        // int r = n-2;
+        // while(l<=r){
+        //     int mid = l + (r-l)/2;
+        //     if(arr[mid]>arr[mid-1] && arr[mid]>arr[mid+1]){
+        //         idx = mid;
+        //         return mid;
+        //     }
+        //     else if(arr[mid]>arr[mid+1]){
+        //         r = mid -1;
+        //     }else{
+        //         l =mid+1;
+        //     }
+        // }
         return idx;
     }
 };
