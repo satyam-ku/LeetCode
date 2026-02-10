067beaf85852049d2a3ef0e2197a01372673c019
Satyam Kumar <151944123+satyam-ku@users.noreply.github.com>
2026-02-11 00:22:11 +0530

    Time: 8 ms (12.35%), Space: 7.9 MB (21.07%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.cpp
@@ -1,15 +1,26 @@
 class Solution {
 public:
+    bool isPs(int x){
+        int root = sqrt(x);
+        if(root*root==x) return true;
+        else return false;
+    }
     bool judgeSquareSum(int c) {
-        long long x = 0;
-        long long y = (long long)sqrt(c);
+        int x = 0;
+        int y = c;
+        while(x<=y){
+            if (isPs(x) && isPs(y)) {
 
-        while (x <= y) {
-            long long sum = x*x + y*y;
-
-            if (sum == c) return true;
-            else if (sum < c) x++; 
-            else y--;
+                return true;
+            }
+            else if (!isPs( y)){
+                y = (int)sqrt(y) * (int)sqrt(y);
+                x=c-y;
+            }
+            else{
+                x = ((int)sqrt(x)+1)*((int)sqrt(x)+1);
+                y =c-x;
+            }
         }
         return false;
     }
