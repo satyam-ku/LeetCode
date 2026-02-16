ed95433b04c96680fb150522a458fb98b04eb0df
Satyam Kumar <151944123+satyam-ku@users.noreply.github.com>
2026-02-16 12:32:18 +0530

    Time: 7 ms (94.82%), Space: 84.3 MB (14.44%) - LeetHub

--- 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
+++ 1833-maximum-ice-cream-bars/1833-maximum-ice-cream-bars.cpp
@@ -1,17 +1,44 @@
 class Solution {
 public:
     int maxIceCream(vector<int>& costs, int coins) {
-        sort(costs.begin(),costs.end());
-        int tot =0;
-        for(int i = 0; i < costs.size(); i++) {
-            if(coins >= costs[i]) {
-                coins -= costs[i];
-                tot++;
-            } else {
-                break;
+        // sort(costs.begin(),costs.end());
+        // int tot =0;
+        // for(int i = 0; i < costs.size(); i++) {
+        //     if(coins >= costs[i]) {
+        //         coins -= costs[i];
+        //         tot++;
+        //     } else {
+        //         break;
+        //     }
+        // }
+        // return tot;
+
+        int maxCost = 0;
+
+       
+        for(int cost : costs) {
+            maxCost = max(maxCost, cost);
+        }
+
+       
+        vector<int> freq(maxCost + 1, 0);
+
+        for(int cost : costs) {
+            freq[cost]++;
+        }
+
+        int total = 0;
+
+       
+        for(int cost = 1; cost <= maxCost; cost++) {
+            while(freq[cost] > 0 && coins >= cost) {
+                coins -= cost;
+                freq[cost]--;
+                total++;
             }
         }
-        return tot;
+
+        return total;
 
     }
 };
