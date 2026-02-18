d3209b15f34c0a3055b5a3b75ac9d248128af96f
Satyam Kumar <151944123+satyam-ku@users.noreply.github.com>
2026-02-18 08:53:44 +0530

    Time: 3 ms (83.15%), Space: 21.9 MB (7.59%) - LeetHub

---
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    int bestClosingTime(string c) {
+        int n = c.length();
+        vector<int> pre(n+1);
+         vector<int> suf(n+1);
+        pre[0] =0;
+        for(int i=0;i<n;i++){
+            pre[i+1] = pre[i] + ((c[i]=='N') ? 1 : 0);
+        }
+        suf[n] = 0;
+        for(int i=n-1;i>=0;i--){
+            suf[i] = suf[i+1] + ((c[i]=='Y') ? 1 : 0);
+        }
+        int minP = n;
+        for(int i=0;i<=n;i++){
+            pre[i] += suf[i];
+            int pen = pre[i];
+            minP = min(pen,minP);
+        }
+        for(int i=0;i<=n;i++){
+            int pen = pre[i];
+            if(pen==minP) return i;
+        }
+        return n;
+
+    }
+};
