711aad771e538a443a4f71a009f134874dacebe9
Satyam Kumar <151944123+satyam-ku@users.noreply.github.com>
2026-02-18 09:40:07 +0530

    Time: 0 ms (100%), Space: 12.3 MB (53.66%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -3,11 +3,14 @@ public:
     int maxSatisfaction(vector<int>& satisfaction) {
         int n = satisfaction.size();
         sort(satisfaction.begin(),satisfaction.end());
+
         vector<int> suf(n);
         suf[n-1] = satisfaction[n-1];
+
         for(int i=n-2;i>=0;i--){
             suf[i] += suf[i+1] + satisfaction[i];
         } 
+
         int idx = -1;
         for(int i=0;i<n;i++){
             if(suf[i]>=0){
@@ -15,7 +18,9 @@ public:
                 break;
             }
         }
+
         if(idx == -1) return 0;
+        
         int x = 1;
         int sum = 0;
         for(int i=idx;i<n;i++){
