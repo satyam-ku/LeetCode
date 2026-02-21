fb6f4d093043e789df8b435c64ed88ca90362240
Satyam Kumar <151944123+satyam-ku@users.noreply.github.com>
2026-02-21 23:49:43 +0530

    Time: 0 ms (100%), Space: 15.9 MB (11.29%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,41 +1,37 @@
 class Solution {
 public:
-   void merge(vector<int>& a, vector<int>& b, vector<int>& v){
-    int i = 0, j = 0, k = 0;
-    while(i < a.size() && j < b.size()){
-        if(a[i] < b[j]){
+    void merge(vector<int>& a, vector<int>& b, vector<int>& v){
+        int i=0, j=0, k=0;
+        while(i<a.size() && j<b.size()){
+            if(a[i]<b[j]){
+                v[k++] = a[i++];
+            }else{
+                v[k++] = b[j++];
+            }
+        }
+        while(i<a.size()){
             v[k++] = a[i++];
         }
-        else{
+        while(j<b.size()){
             v[k++] = b[j++];
         }
     }
-   
-    while(i < a.size()){
-        v[k++] = a[i++];
-    }
-  
-    while(j < b.size()){
-        v[k++] = b[j++];
-    }
-    }
     void sortColors(vector<int>& nums) {
         int n = nums.size();
-    if(n==1) return;
-    int p = n/2; 
-    int q = n- n/2;
-    vector<int> a(p), b(q);
+        if(n==1) return;
+        int n1 = n/2;
+        int n2 = n - n/2;
+        vector<int> a(n1), b(n2);
+        for(int i=0;i<n1;i++){
+            a[i] = nums[i];
+        }
+        for(int i=0;i<n2;i++){
+            b[i] = nums[n1+i];
+        }
+        sortColors(a);
+        sortColors(b);
+        merge(a,b,nums);
+        a.clear(); b.clear();
 
-    for(int i=0;i<p;i++){
-        a[i] = nums[i];
-    } 
-    for(int i=0;i<q;i++){
-        b[i] = nums[i + p];
-    }
-    sortColors(a);
-    sortColors(b);
-    merge(a,b,nums);
-    a.clear();
-    b.clear();
     }
 };
