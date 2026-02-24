ec8191d73a28625c53abc0277d8c7aa75bc6f5aa
Satyam Kumar <151944123+satyam-ku@users.noreply.github.com>
2026-02-24 14:42:36 +0530

    Time: 0 ms (100%), Space: 8.4 MB (71.46%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    string clearDigits(string s) {
+        string st;
+
+        for (char c : s) {
+            if (isdigit(c)) {
+                if (!st.empty()) {
+                    st.pop_back(); 
+                }
+            } 
+            else {
+                st.push_back(c);   
+            }
+        }
+
+        return st;
+    }
+};
